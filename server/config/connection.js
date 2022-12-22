d67fe3b11fdab0eda5027e55f6ac84d16f0198cc
BRIAN LOCKHART <bslockhart@uncg.edu>
2022-12-21 19:11:55 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:30000/brians-book-search-engine", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-}).catch ((err) => {
+}).catch((err) => {
   console.error(err);
 })
 
